b3f0b6eb518e0b028d7c73b9433b20237ced125f
John Herreño <jedihe@gmail.com>
2020-02-15 21:37:43 -0500

    Inject HOST_UID var into the 'web' service container, resolve to id -u via init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,5 @@
 mkdir -p volumes/ossn volumes/ossn_data
 
 git clone --branch 5.2 https://github.com/opensource-socialnetwork/opensource-socialnetwork.git volumes/ossn
+
+sed -i.bak "s/VAR_HOST_UID/$(id -u)/g" docker-compose.yml && rm docker-compose.yml.bak
